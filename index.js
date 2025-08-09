eb22ecf80666978704f94fd5059b01a23309c9ec
Rishika Kherniwal <rishikakherniwal0809@gmail.com>
2025-08-09 21:31:35 +0530

    changes in backend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import nlpRoutes from './routes/nlp.js'
+import uploadRoutes from './routes/upload.js';
 
 dotenv.config()
 const app = express()
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/upload', uploadRoutes);
 
 app.use('/api/nlp', nlpRoutes)
 
